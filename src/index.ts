0fcb0db037a08645988f8e05b9a5e18a991db2b8
Pavel Martynov <25pashok@mail.ru>
2025-04-07 09:41:26 +0500

    feat: добавлена документация и подписки на события. так как классы не реализованы, самих выбрасываний событий пока нет

--- src/index.ts
+++ src/index.ts
@@ -45,3 +45,90 @@ const successModalView = new SuccessModalView(eventEmitter);
 
 //Получение карточек каталога
 catalogModel.fillCatalog() //асинхронная функция
+eventEmitter.on(settings.updateCatalog, (products: IProduct[]) => {
+    catalogView.render(products);
+})
+
+
+//Открытие карточки
+eventEmitter.on(settings.selectProduct, (product: IProduct) => {
+    currentModalType = CurrentModalType.Card;
+    const isInBasket = basketModel.hasItem(product)
+    const cardModalElement = cardModalView.render(product, isInBasket)
+    modalView.render(cardModalElement)
+    modalView.open()
+})
+
+
+//Корзина
+basketButton.addEventListener('click', () => {
+    eventEmitter.emit(settings.openBasket)
+})
+
+eventEmitter.on(settings.addToBasket, (product: IProduct) => {
+    basketModel.addItem(product)
+})
+
+eventEmitter.on(settings.removeFromBasket, (product: IProduct) => {
+    basketModel.removeItem(product)
+})
+
+eventEmitter.on(settings.updateBasket, (products: IProduct[]) => {
+    basketCounter.textContent = `${products.length}`;
+    if (currentModalType === CurrentModalType.Basket) {
+        const basketModalElement = basketModalView.render(products, basketModel.getTotalPrice());
+        modalView.render(basketModalElement);
+    }
+})
+
+eventEmitter.on(settings.openBasket, () => {
+    currentModalType = CurrentModalType.Basket;
+    const basketModalElement = basketModalView.render(basketModel.getItems(), basketModel.getTotalPrice());
+    modalView.render(basketModalElement);
+    modalView.open();
+})
+
+
+//Заказ
+eventEmitter.on(settings.openOrder, () => {
+    currentModalType = CurrentModalType.Order;
+    const orderModalElement = orderModalView.render();
+    modalView.render(orderModalElement);
+    modalView.open();
+})
+
+eventEmitter.on(settings.orderDataConfirmed, (data: IOrderData) => {
+    orderModel.setOrderData(data); //тут в заказ добавляется способ оплаты и адрес
+    currentModalType = CurrentModalType.Contacts;
+    const contactsModalElement = contactsModalView.render();
+    modalView.render(contactsModalElement);
+    modalView.open();
+})
+
+eventEmitter.on(settings.contactsDataConfirmed, async (data: IOrderData) => {
+    orderModel.setOrderData({ //а уже тут остальное
+        email: data.email,
+        phone: data.phone,
+        items: basketModel.getItems(),
+        totalPrice: basketModel.getTotalPrice()
+    });
+    try {
+        const orderResponse = await orderModel.sendOrder();
+        console.log('Заказ успешно оформлен:', orderResponse);
+
+        currentModalType = CurrentModalType.Success;
+        const successModalElement = successModalView.render(basketModel.getTotalPrice());
+        modalView.render(successModalElement);
+        modalView.open();
+        basketModel.clearItems();
+        orderModalView.reset();
+        contactsModalView.reset();
+    } 
+    catch (error) {
+        console.error('Ошибка оформления заказа:', error);
+    }
+})
+
+eventEmitter.on(settings.closeSuccess, () => {
+    modalView.close()
+})
